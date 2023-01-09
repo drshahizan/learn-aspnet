fa334ce58c31be58292ed76875eed41a358ed526
drshahizan <shahizan@utm.my>
2023-01-09 14:25:39 +0800

    Update lab: Cookie

---
+++ lab/cookie-session/LabSession/LabSession/frmCookie.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="frmCookie.aspx.cs" Inherits="LabSession.frmCookie" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Cookie</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <h1>Cookie</h1>
+        <div>
+            <asp:Button ID="btnCreate" runat="server" Text="Create Cookie" OnClick="btnCreate_Click" />&nbsp;<asp:TextBox ID="txtCreateCookie" runat="server"></asp:TextBox>
+            <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+            <br />
+            <br />
+            <asp:Button ID="btnRetrieve" runat="server" Text="Retrive Cookie" OnClick="btnRetrieve_Click" />&nbsp;<asp:TextBox ID="txtRetrieve" runat="server"></asp:TextBox>
+        </div>
+        <p>
+            *Try to retrieve it again after 2 minutes; it will give us a message in the box, as “We don’t found any Cookie”.
+
+        </p>
+    </form>
+</body>
+</html>
---
+++ lab/cookie-session/LabSession/LabSession/frmCookie.aspx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace LabSession
+{
+    public partial class frmCookie : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnCreate_Click(object sender, EventArgs e)
+        {
+            Response.Cookies["name"].Value = txtCreateCookie.Text;
+            Response.Cookies["name"].Expires = DateTime.Now.AddMinutes(2);
+            Label1.Text = "Cookie has been Created";
+            txtCreateCookie.Text = "";
+        }
+
+        protected void btnRetrieve_Click(object sender, EventArgs e)
+        {
+            if (Request.Cookies["name"] == null)
+            {
+                txtRetrieve.Text = "We don’t found any Cookie";
+            }
+            else
+            {
+                txtRetrieve.Text = Request.Cookies["name"].Value;
+            }
+        }
+    }
+}
