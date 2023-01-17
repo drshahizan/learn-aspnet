1d0fe350088f03f196fc2fdd27470750c67d7940
QuixDevf <zunnur.brad@gmail.com>
2023-01-17 20:13:33 +0800

    Project Template

---
+++ project/GroupCatchy/PLASS/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PLASS.Startup))]
+namespace PLASS
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
