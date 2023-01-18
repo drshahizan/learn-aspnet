af67e4930bec8bd097ba147e109c601a90537ab9
duncansez <106868760+duncansez@users.noreply.github.com>
2023-01-18 23:33:07 +0800

    Add table scripts

---
+++ project/GroupAinon/source code/scripts/2. dbo.tblTeam.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[tblTeam]
+(
+	[Id] BIGINT NOT NULL PRIMARY KEY, 
+    [name] BIGINT NOT NULL, 
+    [created_date] DATETIME NULL, 
+    [created_by] BIGINT NULL,
+	[updated_date] DATETIME NULL, 
+    [updated_by] BIGINT NULL,
+	FOREIGN KEY (created_by) REFERENCES tblUser(id),
+	FOREIGN KEY (updated_by) REFERENCES tblUser(id)
+)
