361fa38921c19cfab5c678ddfde3716b3c5ce8ce
QuixDevf <zunnur.brad@gmail.com>
2023-01-24 11:38:25 +0800

    Add position data

---
+++ project/GroupCatchy/database/dbo.position.data.sql
@@ -0,0 +1,12 @@
+SET IDENTITY_INSERT [dbo].[position] ON
+INSERT INTO [dbo].[position] ([id], [name], [category]) VALUES (1, N'goalkeeper', N'defender')
+INSERT INTO [dbo].[position] ([id], [name], [category]) VALUES (2, N'fullback', N'defender')
+INSERT INTO [dbo].[position] ([id], [name], [category]) VALUES (3, N'centerback', N'defender')
+INSERT INTO [dbo].[position] ([id], [name], [category]) VALUES (4, N'sweeper', N'defender')
+INSERT INTO [dbo].[position] ([id], [name], [category]) VALUES (5, N'defending', N'midfielder')
+INSERT INTO [dbo].[position] ([id], [name], [category]) VALUES (6, N'center', N'midfielder')
+INSERT INTO [dbo].[position] ([id], [name], [category]) VALUES (7, N'wingers', N'midfielder')
+INSERT INTO [dbo].[position] ([id], [name], [category]) VALUES (8, N'attacking', N'midfielder')
+INSERT INTO [dbo].[position] ([id], [name], [category]) VALUES (9, N'striker', N'forward')
+INSERT INTO [dbo].[position] ([id], [name], [category]) VALUES (10, N'center forward', N'forward')
+SET IDENTITY_INSERT [dbo].[position] OFF
